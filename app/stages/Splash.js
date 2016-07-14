c1017465e30ae36a2a800cd12d1b54cbbbc41329
Alejandro Gonzalez Sole <guatedude@gmail.com>
2016-07-14 02:36:52 -0400

    Added animations to splash screen

--- app/stages/Splash.js
+++ app/stages/Splash.js
@@ -1,39 +1,28 @@
-import {Canvas, Stage, Sprite, Animation} from 'bunny.2d';
+import {Canvas, Stage, Sprite} from 'bunny.2d';
 
 export default class Splash extends Stage {
   constructor() {
     super(0xFFFFFF);
     this.logo = new Sprite('logo.png');
-    this.logo.alpha = 0;
-    this.logo.position.x = -50;
     this.addChild(this.logo);
 
-    const temp = new Animation({
-      loops: -1
-
+    this.logo.animator.add('logo-fade-in', {
+      keys: [
+        {
+          key: {
+            alpha: 0,
+            position: { x: -50}
+          }
+        }, {
+          key: {
+            alpha: 1,
+            position: { x: 0 }
+          },
+          easing: 'easeOutQuad',
+          duration: 800
+        }
+      ]
     });
-    temp.key({
-      alpha: 1,
-      position: { x: 0 }
-    });
-    temp.key({
-      position: { x: 200 }
-    });
-    temp.key({
-      alpha: 0.5
-    });
-    this.logo.animator.add('logo-fade-in', temp);
-    // this.logo.animator.add('logo-fade-in', {
-    //   loop: true,
-    //   steps: [
-    //     {
-    //       from: { position: { x: 0}},
-    //       to: {position: {x: 200 }},
-    //       duration: 800,
-    //       easing: 'linear'
-    //     }
-    //   ]
-    // });
   }
 
   onReady() {
