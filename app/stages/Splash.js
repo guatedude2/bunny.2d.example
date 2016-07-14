5555cce06985a1c21a99783da9bebe71b9a2365e
Alejandro Gonzalez <jgonzalezsole@appnexus.com>
2016-07-14 18:05:15 -0400

    Added animations and updated Splash

--- app/stages/Splash.js
+++ app/stages/Splash.js
@@ -1,46 +1,44 @@
-import {Canvas, Stage, Sprite} from 'bunny.2d';
+import {Canvas, Stage, Sprite, Loader} from 'bunny.2d';
+import logoAnimations from '../animations/logo.json';
 
 export default class Splash extends Stage {
   constructor() {
     super(0xFFFFFF);
-    this.logo = new Sprite('logo.png');
-    this.addChild(this.logo);
 
-    this.logo.animator.add('logo-fade-in', {
-      keys: [
-        {
-          key: {
-            alpha: 0,
-            position: { x: -50}
-          }
-        }, {
-          key: {
-            alpha: 1,
-            position: { x: 0 }
-          },
-          easing: 'easeOutQuad',
-          duration: 800
-        }
-      ]
-    });
+    this.logo = new Sprite('logo.png');
+    this.logo.animator.load(logoAnimations);
+    this.addChild(this.logo);
   }
 
   onReady() {
-    this.logo.animator.play('logo-fade-in');
+    this.logo.animator.play('fade-in', () => {
+      this.startLoading();
+    });
   }
 
-  startLoader() {
-    //LOAD ASSETS
+  startLoading() {
+    const loader = new Loader();
+    loader.on('progress', this.loadingProgress.bind(this));
+    loader.on('complete', this.loadingComplete.bind(this));
+
+    loader.add('bunny', 'sprites/bunny.png');
+
+    loader.load();
   }
 
-  showMain() {
-    Canvas.setStage('main');
+  loadingProgress(loader) {
+    console.log('PROGRESS', loader.progress)
+  }
+
+  loadingComplete() {
+    console.log('DONE')
+
+    this.logo.animator.play('fade-out', () => {
+      Canvas.setStage('main');
+    });
   }
 
   render() {
     super.render();
-  }
-
-  onMouseDown() {
   }
 };
