434ca17cac0ccd715c7c5eda33b17c5ef5f995c3
Alejandro Gonzalez Sole <guatedude@gmail.com>
2016-07-15 02:34:14 -0400

    Fixes animations and finishes splash

--- app/stages/Main.js
+++ app/stages/Main.js
@@ -1,14 +1,16 @@
-import {Stage, Keys, Point} from 'bunny.2d';
+import {Stage, Keys, Point, TilingSprite, Time} from 'bunny.2d';
 
 import Bunny from '../sprites/Bunny';
 
 export default class Main extends Stage {
   constructor() {
-    super(0x0000FF);
+    super(0xEEEEEE);
     this.bunny = new Bunny();
-    this.goto = new Point(0,0);
 
-    this.addChild(this.bunny);
+    this.map = new TilingSprite('logo.png', this.width, this.height);
+    this.addChildAt(this.map, 0);
+
+    this.world.addChild(this.bunny);
     this.path = [
       new Point(100, 0),
       new Point(100, 100),
@@ -20,23 +22,24 @@ export default class Main extends Stage {
   }
 
   render() {
+    this.map.tilePosition.set(-this.camera.position.x, -this.camera.position.y);
     super.render();
     if (Keys.isPressed(Keys.KEY_LEFT)) {
-      this.bunny.position.x -= 5;
+      this.bunny.position.x -= 500 * Time.deltaTime;
     } else if (Keys.isPressed(Keys.KEY_RIGHT)) {
-      this.bunny.position.x += 5;
+      this.bunny.position.x += 500 * Time.deltaTime;
     }
 
     if (Keys.isPressed(Keys.KEY_UP)) {
-      this.bunny.position.y -= 5;
+      this.bunny.position.y -= 500 * Time.deltaTime;
     } else if (Keys.isPressed(Keys.KEY_DOWN)) {
-      this.bunny.position.y += 5;
+      this.bunny.position.y += 500 * Time.deltaTime;
     }
   }
 
   onMouseDown(e) {
     this.camera.followObject(this.bunny, {
-      distance: 100
+      distance: 200
     });
     // this.camera.followPath(this.path, {
     //   loop: true
--- app/stages/Splash.js
+++ app/stages/Splash.js
@@ -1,18 +1,41 @@
-import {Canvas, Stage, Sprite, Loader} from 'bunny.2d';
-import logoAnimations from '../animations/logo.json';
+import {Canvas, Stage, Sprite, Loader, Shape} from 'bunny.2d';
+import basicAnimations from '../animations/basic.json';
 
 export default class Splash extends Stage {
   constructor() {
     super(0xFFFFFF);
 
+    // logo sprite
     this.logo = new Sprite('logo.png');
-    this.logo.animator.load(logoAnimations);
+    this.logo.alpha = 0;
+    this.logo.animator.load(basicAnimations);
     this.addChild(this.logo);
+
+    // loading bar outter shape
+    this.barOuter = new Shape.Rectangle(404, 24);
+    this.barOuter.alpha = 0;
+    this.barOuter.color = 0xDDDDDD;
+    this.barOuter.position.y = 250;
+    this.barOuter.animator.load(basicAnimations);
+    this.addChild(this.barOuter);
+
+    // loading bar inner shape
+    this.bar = new Shape.Rectangle(0, 20);
+    this.bar.alpha = 0;
+    this.bar.anchor.x = 0;
+    this.bar.position.x = -200;
+    this.bar.position.y = 250;
+    this.bar.color = 0x333333;
+    this.bar.animator.load(basicAnimations);
+    this.addChild(this.bar);
   }
 
   onReady() {
-    this.logo.animator.play('fade-in', () => {
-      this.startLoading();
+    this.logo.animator.play('slideAndFadeIn', () => {
+      this.barOuter.animator.play('fadeIn', () => {
+        this.bar.alpha = 1;
+        this.startLoading();
+      });
     });
   }
 
@@ -27,13 +50,13 @@ export default class Splash extends Stage {
   }
 
   loadingProgress(loader) {
-    console.log('PROGRESS', loader.progress)
+    this.bar.width = loader.progress / 100 * 400;
   }
 
   loadingComplete() {
-    console.log('DONE')
-
-    this.logo.animator.play('fade-out', () => {
+    this.barOuter.animator.play('fadeOut');
+    this.bar.animator.play('fadeOut');
+    this.logo.animator.play('fadeOut', () => {
       Canvas.setStage('main');
     });
   }
